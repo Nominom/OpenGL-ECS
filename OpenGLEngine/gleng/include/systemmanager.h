4c468f64ce8745bcfe34392cff9189f1ebff682e
Nom <j00nzuj@gmail.com>
2019-02-04 18:59:11 +0200

    systemmanager changed list to vector

--- OpenGLEngine/gleng/include/systemmanager.h
+++ OpenGLEngine/gleng/include/systemmanager.h
@@ -35,7 +35,7 @@ public:
 
 class SystemManager {
 private:
-	std::list<IComponentSystemDataInjector*> systemInjectors;
+	std::vector<IComponentSystemDataInjector*> systemInjectors;
 
 public:
 	template <class ...Args>
